3b01f7de4a6f0788f3d8918695d5058361acf251
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-09-05 17:28:27 -0400

    fixed requirements

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
 import requests
 scaler = StandardScaler()
 from fastapi import FastAPI
